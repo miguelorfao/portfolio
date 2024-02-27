f36d725a42c42bc7ea678680668e983127a33c3c
miguelorfao <69687296+miguelorfao@users.noreply.github.com>
2024-02-27 23:46:52 +0000

    update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(
   cors({
-    origin: ["https://miguelorfao.onrender.com/"],
+    origin: ["https://portfolio-xahb.onrender.com"],
     methods: ["POST", "GET"],
   })
 );
