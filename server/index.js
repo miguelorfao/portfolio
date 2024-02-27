1ac249cb3bed25599f72f7365dc73a68c20dcab6
miguelorfao <69687296+miguelorfao@users.noreply.github.com>
2024-02-27 23:09:51 +0000

    update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(
   cors({
-    origin: ["https://miguel-three.vercel.app"],
+    origin: ["https://miguelorfao.onrender.com/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
